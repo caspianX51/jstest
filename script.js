23d7e12aa07b282cff00d39cf91ba7a7a68087c3
Oslan VR <alanosmarvira@gmail.com>
2023-05-14 12:40:56 -0600

    Boilerplate

---
+++ script.js
@@ -0,0 +1,8 @@
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+function XO(str) {
+  //code here
+}
